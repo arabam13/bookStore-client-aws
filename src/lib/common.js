ad3484ec479582ffd8bb345f9d86dbad6116829d
araba <araba.mohamed@gmail.com>
2024-03-06 12:32:04 +0100

    updating common.js

--- src/lib/common.js
+++ src/lib/common.js
@@ -36,7 +36,7 @@ export async function getAuthenticatedUser() {
 
 export async function getBooks() {
   try {
-    console.log(API_ROUTES.BOOKS);
+    // console.log(API_ROUTES.BOOKS);
     const response = await axios({
       method: "GET",
       url: `${API_ROUTES.BOOKS}`,
